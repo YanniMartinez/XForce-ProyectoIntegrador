cdc2d503ebc370b1afe1b2f29a6f7fbae711882e
proivited <miguel.alexis.gomez.a@gmail.com>
2021-07-02 13:03:06 -0500

    Se acualiza por defecto pag principal

--- Assets/js/Card.js
+++ Assets/js/Card.js
@@ -77,7 +77,8 @@ class Card{
 
 }
 
-
+const Fuego = new Card("electric"); 
+Fuego.getInfo()
 
 /* Probando */    
 /*const Pikachu = new Card("pikachu"); //Declaramos un objeto tipo card y le mandamos como etiqueta "pikachu", esto de forma más profesional puede ser la categoria general como "hombre, mujer, etc".
