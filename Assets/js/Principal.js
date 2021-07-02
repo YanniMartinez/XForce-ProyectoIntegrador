a71ac9b0770aee9a0ca7c0c90ec811fc45b002c5
proivited <miguel.alexis.gomez.a@gmail.com>
2021-07-02 12:44:49 -0500

    El carrusel llena de manera automatica las etiquetas

---
+++ Assets/js/Principal.js
@@ -0,0 +1,4 @@
+import Card from "./Card";
+/* Probando */
+const Pikachu = new Card("pikachu"); //Declaramos un objeto tipo card y le mandamos como etiqueta "pikachu", esto de forma más profesional puede ser la categoria general como "hombre, mujer, etc".
+Pikachu.getInfo();
