ae27cc5955b66cf9f6d5fab04b8b6cce7fcd9b87
jaquelin <a.jaquelinperez@gmail.com>
2021-07-06 16:58:06 -0500

    Se agregaron las categorias laterales de forma dinamica

---
+++ Assets/js/check-articulos.js
@@ -0,0 +1,83 @@
+//Clase para el objeto tarjeta
+class Check
+{
+    
+    #nombre="";
+    #etiqueta="";
+
+    constructor(nombre)
+    {
+        this.#nombre=nombre;
+
+    }
+
+    //Siver para poner los valores a los atributos principales del objeto
+    
+    set nombre(value){this.#nombre=value;}
+    
+
+    //Obtenemos los valores
+    get nombre(){return this.#nombre;}
+    
+
+    /**
+     *  <div class="col-sm-3" >
+     * <div class="form-check">
+                        <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="Arts">
+                        <label class="form-check-label" for="blankCheckbox">
+                            Art & Design
+                        </label>
+                    </div>
+     */
+
+    crearCategorias(i)
+    {
+        
+
+
+        let div2=document.createElement("div")
+        div2.classList="form-check";
+
+        let input=document.createElement("input");
+        input.classList="form-check-input position-static";
+        input.type="checkbox";
+        input.id="blankCheckbox"+i;
+        input.value=this.#nombre;
+        
+
+        let label=document.createElement("label");
+        label.classList="form-check-label";
+        label.for="blankCheckbox";
+        label.textContent=this.#nombre.toUpperCase();
+
+        
+        
+
+        div2.appendChild(input);
+        div2.appendChild(label);
+        
+
+
+        return div2;
+
+    }
+    
+}
+
+function insertarCategorias(categorias){
+    let p=document.querySelector("#categoriaslat");
+    categorias.forEach((check,i) => {
+        p.appendChild(check.crearCategorias(i));
+    });
+}
+
+let checks=[];
+
+let array = ["electrodomesticos", "ropa","computo","celulares","videojuegos"];
+array.forEach(element => {
+    let checkbox=new Check(element);
+    checks.push(checkbox);
+});
+
+
+insertarCategorias(checks); 
