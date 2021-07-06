ca1c2ac139cdd52baad787fe292e9344de58fdbe
Yann <metalyanni@gmail.com>
2021-07-06 18:22:57 -0500

    Implementamos la conexión del boton de busqueda en función del nombre que reciba(en este caso es arbitrario)

--- Assets/js/descripcionArticulo.js
+++ Assets/js/descripcionArticulo.js
@@ -27,8 +27,13 @@ class Card
     get descripcion(){return this.#descripcion;}
     get etiqueta(){return this.#etiqueta;}
 
+    /**
+     * Permite la creación de etiquetas necesarias para el card
+     * @returns div Retorna una etiqueta div con todo el contenido basado en el objeto o en el pokemon
+     */
     crearCard()
     {
+        console.log("*********** ");
         let div= document.createElement("div");
         div.classList="card p-3  border-0";
 
@@ -62,24 +67,17 @@ class Card
     }
 }
 
+/**
+ * Agrega un hijo al id Card-group-1
+ * @param {*} cards 
+ */
 function insertCards(cards){
 
-        document.querySelector(`#card-group-1`).appendChild(cards[0].crearCard());
+    document.querySelector(`#card-group-1`).appendChild(cards[0].crearCard());
     
 }
 
-function loadCards(){
-    let obj = [];
-    let categoria = "water"; 
-    fetch(`https://workshop-mongo.herokuapp.com/pokemon/types/${categoria}`)
-    .then(resp => resp.json())
-    .then(data => {
-        let cards = jsonToCard(data);
-        console.log(cards);
-        insertCards(cards);
-    });
-}
-
+//Se mantuvo igual
 function loadCards(categoria){
 
     fetch(`https://workshop-mongo.herokuapp.com/pokemon/types/${categoria}`)
@@ -91,6 +89,7 @@ function loadCards(categoria){
     });
 }
 
+//Se mantuvo igual
 function jsonToCard(data){
     let cards = [];
     
@@ -100,4 +99,50 @@ function jsonToCard(data){
     return cards;
 }
 // document.body.onload = loadCards();
-loadCards("water");
+loadCards("water"); //Despliega un sólo elemento de tipo AGUA
+
+//***************Haciendo una relación del boton search con el espacio de cards********************** */
+
+/**
+ * Hace una consulta FETCH (en este caso a la API de pokemon), dentro de ella manda a llamar a la función 
+ * jsonToCard que crea un objeto y retorna un arreglo de objetos (en este caso solo es uno).
+ * Posteriormente manda a llamar a la función insertCard(arregloDeObjetos) donde insertará un hijo en el 
+ * div con id card-group-1
+ * @param {*} nombre Hace referencia al nombre del pokemon que buscará en el API.
+ */
+function loadCardName(nombre){
+
+    fetch(`https://workshop-mongo.herokuapp.com/pokemon/name/${nombre}`)
+    .then(resp => resp.json())
+    .then(data => {
+        //console.log("El nombre fue" + data[0].name + " :D");
+        let cards = jsonToCard(data); //Convierte el JSON a objeto CARD
+        //console.log(cards);
+        insertCards(cards); //Inserta el objeto en un div
+    });
+}
+
+/**
+ * Le Decimos ubique el elemento con el id "Card-group-1" y en él agregue como hijo todo lo indicado en la 
+ * clase crearCard.
+ * @param {*} cards Es el arreglo de objetos tipo Card, es decir toda la información de la imagen, del nombre
+ * de la descripción y evoluciones
+ */
+function insertCard(cards){
+    console.log("pikachu ya mero");
+    document.querySelector(`#card-group-1`).appendChild(cards[0].crearCard());
+
+    
+}
+
+/**
+ * Relacionamos una variable botón con el boton cuyo ID es botonBusqueda
+ * HAcemos un EventListener que al apretarse nos ejecutará la función "loadCardName" la cual nos
+ * desplegará un pokemon con ese nombre en un espacio designado
+ */
+let buttonSearch = document.querySelector("#botonBusqueda");
+buttonSearch.addEventListener("click", event =>{
+    //alert("Q pdo");
+    loadCardName("pikachu");
+
+});
