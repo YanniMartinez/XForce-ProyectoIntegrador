e13af5536486f19e3a3e4906064a407ab6c05548
jaquelin <a.jaquelinperez@gmail.com>
2021-07-22 12:23:42 -0500

    Se agregaron mas articulos al JS gamer e impresoras

--- assets/js/gamer.js
+++ assets/js/gamer.js
@@ -16,5 +16,65 @@ let gamer = [
         "descripcion": "$919.99",
         "categoria": "gamer",
         "imagen": "https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/h/e/headset_chuis_dreamgear_1.jpg"
+    },
+    {
+        "nombre": "F1 2021",
+        "descripcion": "$1,499.00",
+        "categoria": "gamer",
+        "imagen": "https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/image/700x800/9df78eab33525d08d6e5fb8d27136e95/f/1/f1_ps5_nueva_1.jpg"
+    },
+    {
+        "nombre": "Headset inalambrico XBOX negro",
+        "descripcion": "$2,299.00",
+        "categoria": "gamer",
+        "imagen": "https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/x/b/xbx_wireless_headset_1.jpg"
+    },
+    {
+        "nombre": "Consola PlayStation 5",
+        "descripcion": "$13,999.00",
+        "categoria": "gamer",
+        "imagen": "https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/c/o/consola-playstation-5-5_2_2.jpg"
+    },
+    {
+        "nombre": "Consola Xbox Series X",
+        "descripcion": "$16,999.00",
+        "categoria": "gamer",
+        "imagen": "https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/image/700x800/9df78eab33525d08d6e5fb8d27136e95/x/b/xbox1_3_2_1.jpg"
+    },
+    {
+        "nombre": "Grand Theft Auto V Premium Edition",
+        "descripcion": "$599.00",
+        "categoria": "gamer",
+        "imagen": "https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/g/t/gta_5_ps4_1.jpg"
+    },
+    {
+        "nombre": "FIFA 20",
+        "descripcion": "$549.00",
+        "categoria": "gamer",
+        "imagen": "https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/f/e/fefa_20_ps4_1.jpg"
+    },
+    {
+        "nombre": "Resident Evil 3 remake",
+        "descripcion": "$549.00",
+        "categoria": "gamer",
+        "imagen": "https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/r/e/re3_ps4_1.jpg"
+    },
+    {
+        "nombre": "The legend of Zelda links Awakening",
+        "descripcion": "$999.99",
+        "categoria": "gamer",
+        "imagen": "https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/image/700x800/9df78eab33525d08d6e5fb8d27136e95/t/h/the_legend_of_zelda_links_awakening_3.jpg"
+    },
+    {
+        "nombre": "Figura POP Marvel Deadpool Shuerlock Deadpool",
+        "descripcion": "$279.99",
+        "categoria": "gamer",
+        "imagen": "https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/s/h/sherlockpool_1.jpg"
+    },
+    {
+        "nombre": "Figura POP The Falcon And The Winter Soldier John F Walker",
+        "descripcion": "$349.99",
+        "categoria": "gamer",
+        "imagen": "https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/j/h/jhon_walker_cap_1.jpg"
     }
 ]
--- assets/js/impresoras.js
+++ assets/js/impresoras.js
@@ -17,5 +17,67 @@ let impresoras = [
         "descripcion": "$1,493.99",
         "categoria": "impresoras",
         "imagen": "https://quecartucho.es/img/cms/Impresoras/Impresora%20HP%20Officejet%206230.jpg"
+    },
+    {
+        "nombre": "HP Officejet Pro 8210",
+        "descripcion": "$2,199.00",
+        "categoria": "impresoras",
+        "imagen": "https://m.media-amazon.com/images/I/71Wdo2UcwOL._AC_SL1500_.jpg"
+    },
+    {
+        "nombre": "Epson Ecotank et-27200",
+        "descripcion": "$3,798.00",
+        "categoria": "impresoras",
+        "imagen": "https://www.tiendatinta.com/96758-large_default/epson-ecotank-et-2720.jpg"
+    },
+    {
+        "nombre": "HP Envy 6020",
+        "descripcion": "$2,499.00",
+        "categoria": "impresoras",
+        "imagen": "https://quecartucho.es/blog/wp-content/uploads/2020/08/hp-6020-opiniones-1024x493.jpg"
+    },
+    {
+        "nombre": "Epson Workforce WF-2860DWF",
+        "descripcion": "$7,599.99",
+        "categoria": "impresoras",
+        "imagen": "https://www.epson.es/files/assets/converted/1500m-1500m/w/f/2/8/wf2860-2865-25-.jpg."
+    },
+    {
+        "nombre": "HP Envy Photo 6234",
+        "descripcion": "$3,089.00",
+        "categoria": "impresoras",
+        "imagen": "https://infoimpresoras.com/wp-content/uploads/2020/04/hp-Envy-Photo-6230-900x578.jpg"
+    },
+    {
+        "nombre": "HP OfficeJet Pro 9010",
+        "descripcion": "$3,673.50",
+        "categoria": "impresoras",
+        "imagen": "https://product-images.www8-hp.com/digmedialib/prodimg/lowres/c06264857.png"
+    },
+    {
+        "nombre": "Canon Pixma MX475",
+        "descripcion": "$2,506.47",
+        "categoria": "impresoras",
+        "imagen": "https://m.media-amazon.com/images/I/81mLE4QFuUL._AC_SL1500_.jpg"
+    },
+    {
+        "nombre": "Brother MFC-J5330DW",
+        "descripcion": "$3,850.10",
+        "categoria": "impresoras",
+        "imagen": "https://m.media-amazon.com/images/I/71CJezeqv-L._AC_SL1500_.jpg"
+    },
+    {
+        "nombre": "HP PageWide 377DW",
+        "descripcion": "$2,316.04",
+        "categoria": "impresoras",
+        "imagen": "https://www.mastoner.com/images/productos/IMPRESORA-MULTIFUNCION-HP-PAGEWIDE-PRO-377DW.jpg"
+    },
+    {
+        "nombre": "HP INK TANK 115",
+        "descripcion": "$2,742.70",
+        "categoria": "impresoras",
+        "imagen": "https://neoxcorp.com/storage/media/large_arXTv5VnA5lof2FAHgJiiJwod5F9hxqQXORBvxuu.jpg"
     }
+
+    
 ]
