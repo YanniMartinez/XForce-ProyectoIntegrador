10a32d45f2ad4ac74dc65763e6d9472d527ddecc
Yann <metalyanni@gmail.com>
2021-07-22 11:37:37 -0500

    Adaptando API del terror

--- assets/js/cards-json.js
+++ assets/js/cards-json.js
@@ -1,39 +1,4 @@
 [
-    {
-        "nombre": "Laptop Ideapad 110",
-        "descripcion": "$7,000.00",
-        "categoria": "Laptops",
-        "imagen": "bWFzdGVyfHJvb3R8MTAxNTM3fGltYWdlL2pwZWd8aGMzL2gzZi85MjUwNjM3ODA3NjQ2LmpwZ3wzMzdkYjcyMzdiZDNiNjY2Y2QzZjEwZmQ3NjIyMmI2YmUyM2EzYmFmYTJjOTg5ODUwOGY3MjI3N2YyZmNmNTY0"
-    },
-    {
-        "nombre": "",
-        "descripcion": "",
-        "categoria": "",
-        "imagen": "bWFzdGVyfHJvb3R8MTAxNTM3fGltYWdlL2pwZWd8aGMzL2gzZi85MjUwNjM3ODA3NjQ2LmpwZ3wzMzdkYjcyMzdiZDNiNjY2Y2QzZjEwZmQ3NjIyMmI2YmUyM2EzYmFmYTJjOTg5ODUwOGY3MjI3N2YyZmNmNTY0"
-    }
+    
 ]
 
-[
-    {
-        "nombre": "Dell Laptop Inspiron 15.6 Laptop - Intel Core i5 11a Gen - 12GB ddr4 - 256GB SSD",
-        "descripcion": "​Logre más sobre la marcha con el portátil delgado y potente de Dell. El portátil Dell Inspiron 15 3000 está equipado con un potente procesador Intel Core i5-1135G7 Quad-Core, 12 GB de RAM y 256 GB de almacenamiento SSD de almacenamiento. Rápido y eficiente, este dispositivo portátil cuenta con una pantalla IPS Full HD de 15,6 pulgadas.",
-        "categoria": "Laptop",
-        "imagen": "https://m.media-amazon.com/images/I/61zRDADh+YS._AC_SL1500_.jpg"
-
-
-    },
-
-    {
-        "nombre": "Dell Laptop Inspiron 5410 2en1 14 Touch Core i3 11va Gen. 8GB RAM, 256 SSD, Windows 10, Complete Care, Plata.",
-        "descripcion": "Cada Laptop Dell esta conustruida para tus activiadades diarias, con la mejor tecnología, almacenamiento amplio, procesador de ultima generacion y materiales sustentables, la llevarás a todos lados.",
-        "categoria": "Laptop",
-        "imagen": "https://m.media-amazon.com/images/I/614yQP+60LS._AC_SL1280_.jpg" 
-    },
-
-    {
-        "nombre": "Evoo ultra delgada 15.6 FHD Intel Core i7 2.4-3.4GHz ram 8GB ssd 256GB, cámara web 2MP, Windows 10 Home, WiFi Bluetooth, pantalla resolution 1920x1080, laptop color negro",
-        "descripcion": "Procesador Intel Quad Core i7-6660U GHz, pantalla LED Full HD de 15,6 pulgadas (1920 x 1080), 8 GB de RAM, unidad de estado sólido de 256 GB, Windows 10 Home 64 bits, 802.11ac Wi-Fi 5 WLAN, Bluetooth, cámara web, micrófono",
-        "categoria": "Laptop",
-        "imagen": "https://m.media-amazon.com/images/I/71VrP5a-prL._AC_SL1500_.jpg"
-    }
-]
