f0254f251b958518ea42249a726dd5ec02c94f76
generaliru <ucamachoa1600@alumno.ipn.mx>
2021-07-29 17:46:45 -0500

    Arreglando la transiciòn del side bar

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,5 +1,11 @@
-$(document).ready(function () {
-            $('#sidebarCollapse').on('click', function () {
-                $('#sidebar').toggleClass('active');
-            });
-        });
+
+document.querySelector('#sidebarCollapse').addEventListener('click', event => {
+    let sidebar = document.querySelector('#sticky-sidebar');
+    let sidebar1 = document.querySelector('#sidebar');
+    let main = document.querySelector('#main');
+    sidebar1.classList.toggle('active');
+    sidebar.classList.toggle('col');
+    sidebar.classList.toggle('p-1');
+    sidebar.classList.toggle('position-fixed');
+    main.classList.toggle('offset-3');
+});
